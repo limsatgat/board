156a5d3f96cd004e442c6814d7795ebc3f99a4a1
suheon.lim <suheon.lim@lemonhc.com>
2023-02-24 15:27:10 +0900

    코드 수정

--- src/main/java/com/study/board/controller/Main.java
+++ src/main/java/com/study/board/controller/Main.java
@@ -58,7 +58,7 @@ public class Main {
     JunHanService junHanService;
 
     @Autowired
-    JunYeongService junYeongService;
+    JunyeongService junyeongService;
 
     @Autowired
     JiWooService jiWooService;
